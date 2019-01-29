6177f1bbce1dbb5538c017d97901c3961ee7ad4e
John Alves <dev.jonny255d@gmail.com>
2019-01-29 18:14:56 -0200

    Initial commit

---
+++ app/src/main/java/quanticheart/com/notificationsender/NotificationSend.java
@@ -0,0 +1,267 @@
+package quanticheart.com.notificationsender;
+
+import android.app.NotificationChannel;
+import android.app.NotificationChannelGroup;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import java.util.Random;
+
+/*
+ * Copyright(c) Developed by John Alves at 2019/01/08 - BOOMMM!
+ * .
+ */
+
+@SuppressWarnings("all")
+public class NotificationSend {
+
+    //==============================================================================================
+    //
+    // ** Init Vars
+    //
+    //==============================================================================================
+
+    private static final String TAG = "Firebase Cloud MSG";
+
+
+    //==============================================================================================
+    //
+    // ** Show NotificationSend
+    //
+    //==============================================================================================
+
+    /**
+     * @param context     - init
+     */
+    public static void sendNotification(Context context) {
+        showNotification(createNotificationManager(context), createNotificationBuilder(context, "Teste", "Click para abrir o app!!"));
+    }
+
+    //==============================================================================================
+    //
+    // ** Show NotificationSend
+    //
+    //==============================================================================================
+
+    /**
+     * @param notificationManager - sys manager
+     * @param notification        - new notification
+     */
+    private static void showNotification(NotificationManager notificationManager, android.app.Notification notification) {
+        notificationManager.notify(getRamdomID(), notification);
+    }
+
+    //==============================================================================================
+    //
+    // ** Create NotificationSend Builder
+    //
+    //==============================================================================================
+
+    /**
+     * @param context     - init
+     * @param Title       - title
+     * @param messageBody - msg notification
+     * @return new notification
+     */
+    private static android.app.Notification createNotificationBuilder(Context context, String Title, String messageBody) {
+
+        NotificationCompat.WearableExtender wearableExtender =
+                new NotificationCompat.WearableExtender().setHintHideIcon(true);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context)
+//                .setLargeIcon(getLargeIcon(context))
+                .setSmallIcon(getIcon(context))
+                .setColor(getColor())
+                .setContentTitle(Title)
+                .setContentText(messageBody)
+                .setStyle(getBigTextStyle(messageBody))
+                .setAutoCancel(true)
+//                .setSound(getSound(context))
+                .setVibrate(getVibration())
+                .setChannelId(getChannelID(context))
+                .setContentIntent(getPendingIntent(context))
+                .extend(wearableExtender);
+
+        return notificationBuilder.build();
+    }
+
+    //==============================================================================================
+    //
+    // ** Create NotificationSend Manager
+    //
+    //==============================================================================================
+
+    /**
+     * @param context - init
+     * @return - new NotificationManager
+     */
+    private static NotificationManager createNotificationManager(Context context) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(getChannelID(context), getChannelName(), getChannelImportance());
+            notificationChannel.setDescription(getChannelDescription());
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(getChannelLightColor());
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(getVibration());
+            notificationChannel.setBypassDnd(getBypassDND(notificationChannel));
+            notificationChannel.setLockscreenVisibility(getChannelLockscreenVisibility());
+//            notificationChannel.setSound(getSound(context), getAudioAttributes());
+            notificationChannel.setGroup(createNewGroupNotification(context));
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
+        return notificationManager;
+    }
+
+    //==============================================================================================
+    //
+    // ** Create NotificationSend Group in Oreo
+    //
+    //==============================================================================================
+
+    /**
+     * @param context - init
+     * @return new Group NotificationSend for oreo
+     */
+    private static String createNewGroupNotification(Context context) {
+        String groupId = "group_id_11";
+        CharSequence groupName = "Informativos";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannelGroup(new NotificationChannelGroup(groupId, groupName));
+        }
+        return groupId;
+    }
+
+    //==============================================================================================
+    //
+    // ** Utils for Oreo
+    //
+    //==============================================================================================
+
+    private static String getChannelName() {
+        return "Infos";
+    }
+
+    private static String getChannelDescription() {
+        return "Informações sobre a movida e suas promoções";
+    }
+
+    private static String getChannelID(Context context) {
+        return context.getResources().getString(R.string.d_canal);
+    }
+
+    private static int getChannelLightColor() {
+        return Color.YELLOW;
+    }
+
+    /**
+     * IMPORTANCE_MAX: unused
+     * IMPORTANCE_HIGH: shows everywhere, makes noise and peeks
+     * IMPORTANCE_DEFAULT: shows everywhere, makes noise, but does not visually intrude
+     * IMPORTANCE_LOW: shows everywhere, but is not intrusive
+     * IMPORTANCE_MIN: only shows in the shade, below the fold
+     * IMPORTANCE_NONE: a notification with no importance; does not show in the shade
+     *
+     * @return Importance
+     */
+    private static int getChannelImportance() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return NotificationManager.IMPORTANCE_HIGH;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Set whether or not the notification should bypass Do Not Disturb mode (the INTERRUPTION_FILTER_PRIORITY value
+     *
+     * @param notificationChannel
+     * @return true or false
+     */
+    private static boolean getBypassDND(NotificationChannel notificationChannel) {
+        boolean canBypass = getCanBypassDND(notificationChannel);
+        return false;
+    }
+
+    private static boolean getCanBypassDND(NotificationChannel notificationChannel) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return notificationChannel.canBypassDnd();
+        } else {
+            return false;
+        }
+    }
+
+    private static int getChannelLockscreenVisibility() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return android.app.Notification.VISIBILITY_PUBLIC;
+        } else {
+            return 0;
+        }
+    }
+
+    private static AudioAttributes getAudioAttributes() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_GAME)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+        } else {
+            return null;
+        }
+    }
+
+    //==============================================================================================
+    //
+    // ** Utils
+    //
+    //==============================================================================================
+
+    private static int getColor() {
+        return Color.parseColor("#ed7b00");
+    }
+
+    private static Bitmap getLargeIcon(Context context) {
+        return BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher);
+    }
+
+    private static int getIcon(Context context) {
+        return R.mipmap.ic_launcher;
+    }
+
+    private static PendingIntent getPendingIntent(Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+    }
+
+    private static long[] getVibration() {
+        return new long[]{0, 500, 110, 500, 110};
+    }
+
+    private static int getRamdomID() {
+        Random rand = new Random();
+        return rand.nextInt(1000) + 500;
+    }
+
+    //==============================================================================================
+    //
+    // ** NotificationSend Style
+    //
+    //==============================================================================================
+
+    //getStyle BigText
+    private static NotificationCompat.BigTextStyle getBigTextStyle(String mMessage) {
+        return new NotificationCompat.BigTextStyle()
+                .bigText(mMessage);
+    }
+
+}
